79cd1ef99452b149d51ca79d67430e82afb37d53
Scratch <Scrxtchy@users.noreply.github.com>
2018-08-24 15:09:12 +1000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,34 @@
+import ts3lib as ts3
+from ts3plugin import ts3plugin, PluginHost
+import ts3defines
+from PythonQt.QtGui import *
+from PythonQt.QtCore import *
+
+
+class bannerHider(ts3plugin):
+	"""docstring for bannerHidder"""
+	name = "bannerHider"
+	requestAutoload = False
+	version = "1"
+	apiVersion = 22
+	author = "Scratch"
+	description = "Hides the banner on connection"
+	offersConfigure = False
+	commandKeyword = ""
+	infoTitle = ""
+	hotkeys = []
+	menuItems = []
+
+	def getBanner(self):
+		try:
+			for widget in QApplication.instance().allWidgets():
+				if widget.className() == 'Banner':
+					return widget
+		except: from traceback import format_exc; ts3.logMessage(format_exc(), ts3defines.LogLevel.LogLevel_ERROR, "PyTSon", 0)
+	
+	def hideBanner(self, banner):
+		banner.visible = False
+	
+	def onConnectStatusChangeEvent(self, serverConnectionHandlerID, newStatus, errorNumber):
+		if newStatus == 2:
+			self.hideBanner(self.getBanner())
